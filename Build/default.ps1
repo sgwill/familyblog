48faca3b5542896b0cf61bba310b9e502755345c
Sam Williamson <sam.williamson@cqlcorp.com>
2011-09-30 09:06:56 -0400

    working on integrating git into build script

--- Build/default.ps1
+++ Build/default.ps1
@@ -1,11 +1,15 @@
 properties {
   $base_dir = Resolve-Path ..
   $build_dir = "$base_dir\build"
-  $tests_dir = "$base_dir\Tests\"
+  $test_library_dir = "$base_dir\Tests\"
   $sln = "$base_dir\WilliamsonFamily.sln"
-  $debug_dir = "$build_dir\Debug"
-  $test_dir = "$debug_dir\Tests\"
-  $release_dir = "$build_dir\Release\\"
+  
+  $build_debug_dir = "$build_dir\Debug"
+  $test_debug_dir = "$build_debug_dir\Tests\"
+  
+  $build_release_dir = "$build_dir\Release" 
+  $test_release_dir = "$build_release_dir\Tests\"
+  
   $publish_dir = "_PublishedWebsites\$project_path\\"  
   $ftp_user = "Sadajorew"
   $ftp_password = "0417sad"
@@ -25,47 +29,71 @@ task Instructions -Description "Build script instructions" {
 
 task Clean-Debug -Description "Clean debug build output folder" { 
     msbuild $sln "/nologo" "/t:Clean" "/p:Configuration=Debug" "/fileLogger"
-    remove-item -force -recurse "$debug_dir\\" -ErrorAction SilentlyContinue
+    remove-item -force -recurse "$build_debug_dir\\" -ErrorAction SilentlyContinue
 }
 
 task Clean-Release -Description "Clean release build output folder" { 
-    msbuild $sln "/nologo" "/t:Clean" "/p:Configuration=Release" "/p:OutDir=""$release_dir""" "/fileLogger"
-    remove-item -force -recurse "$release_dir\" -ErrorAction SilentlyContinue
+    msbuild $sln "/nologo" "/t:Clean" "/p:Configuration=Release" "/fileLogger"
+    remove-item -force -recurse "$build_release_dir\" -ErrorAction SilentlyContinue
 }
 
 task Clean -depends Clean-Debug,Clean-Release -Description "Clean build output folder" { 
 }
 
 task Init -depends Clean -Description "Ensure folders exist" {
-    new-item $debug_dir -itemType directory
-    new-item $release_dir -itemType directory
+    new-item $build_debug_dir -itemType directory
+    new-item $build_release_dir -itemType directory
+}
+
+task Checkout-Develop -Description "Checkout develop branch" {
+    git checkout develop
+}
+
+task Checkout-Release -Description "Checkout master branch" {
+    git checkout master
 }
 
 task Build-Debug -Description "Build in DEBUG mode" {
-    msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Debug" "/p:OutDir=""$debug_dir""" "/fileLogger"
+    msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Debug" "/fileLogger"
 }
 
 task Build-Release -Description "Build in RELEASE mode" {
-    msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Release" "/p:OutDir=""$release_dir""" "/fileLogger" "/noconsolelogger"
+    msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Release" "/fileLogger" 
 }
 
-task Test -depends Clean-Debug -Description "Runs tests" {
+task Test -depends Checkout-Develop,Clean-Debug,Build-Debug -Description "Runs tests in DEBUG mode" {
 	# Build
-    msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Debug" "/fileLogger"
+    #msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Debug" "/fileLogger"
     
-    # Clean Test folder
-    if (test-path $test_dir) {
-        remove-item -force -recurse "$test_dir\*.*" -ErrorAction SilentlyContinue
-    } else {
-        mkdir $test_dir
+    # Ensure Test Folder
+    $pathExists = test-path $test_debug_dir
+    if (-not $pathExists) {
+        mkdir $test_debug_dir
     }
-    #
     
     # Copy test bins to output folder
-    ls $tests_dir -recurse | where {$_.psIsContainer -eq $true -and $_.Name -eq 'bin' } | foreach { ls $_.fullname } | where { $_.psIsContainer -and $_.Name -eq 'Debug' } | foreach { ls $_.fullname *.dll} | foreach { cp $_.fullname $test_dir }     
+    ls $test_library_dir -recurse | where {$_.psIsContainer -eq $true -and $_.Name -eq 'bin' } | foreach { ls $_.fullname } | where { $_.psIsContainer -and $_.Name -eq 'Debug' } | foreach { ls $_.fullname *.dll} | foreach { cp $_.fullname $test_debug_dir }     
     
     # Test
-	$tests = ls $test_dir *.Tests.dll | foreach { '/testcontainer:' + $test_dir + $_ }
+	$tests = ls $test_debug_dir *.Tests.dll | foreach { '/testcontainer:' + $test_debug_dir + $_ }
+	mstest $tests
+}
+
+task Test-release -depends Clean-Release,Build-Release -Description "Runs tests in RELEASE mode" {
+	# Build
+    #msbuild $sln "/nologo" "/t:Rebuild" "/p:Configuration=Release" "/fileLogger"
+    
+    # Ensure Test Folder
+    $pathExists = test-path $test_release_dir
+    if (-not $pathExists) {
+        mkdir $test_release_dir
+    }
+    
+    # Copy test bins to output folder
+    ls $test_library_dir -recurse | where {$_.psIsContainer -eq $true -and $_.Name -eq 'bin' } | foreach { ls $_.fullname } | where { $_.psIsContainer -and $_.Name -eq 'Release' } | foreach { ls $_.fullname *.dll} | foreach { cp $_.fullname $test_release_dir }     
+    
+    # Test
+	$tests = ls $test_release_dir *.Tests.dll | foreach { '/testcontainer:' + $test_release_dir + $_ }
 	mstest $tests
 }
 
