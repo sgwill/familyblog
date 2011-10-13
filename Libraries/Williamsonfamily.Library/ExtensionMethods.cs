d1927cdce83c23b4f213468182faca78e1f6ec87
Sam Williamson <sam.williamson@cqlcorp.com>
2011-10-13 08:05:43 -0400

    dammit, forgot to add files

---
+++ Libraries/Williamsonfamily.Library/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WilliamsonFamily.Library
+{
+	public static class ExtensionMethods
+	{
+		/// <summary>
+		/// Answers true if this String is neither null or empty.
+		/// </summary>
+		/// <remarks>I'm also tired of typing !String.IsNullOrEmpty(s)</remarks>
+		public static bool HasValue(this string s)
+		{
+			return !string.IsNullOrEmpty(s);
+		}
+	}
+}
