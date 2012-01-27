8637560f39af5e44e91ace41754bf02ae71145a5
sgwill <samuel.g.williamson@gmail.com>
2012-01-26 19:03:59 -0500

    Added notify script to use desktop notifications instead of jsgrowl if available

---
+++ Webs/WilliamsonFamily.Web/Scripts/site.js
@@ -0,0 +1,12 @@
+﻿function notify(message, header, position) {
+    if (window.webkitNotifications) {
+        var notification = window.webkitNotifications.createNotification('', header, message);
+        notification.show();
+        setTimeout(function () {
+            notification.cancel();
+        }, '5000');
+    }
+    else {
+        $.jGrowl(message, { header: header, position: position });
+    }
+}
